1f116da09014c7d9431a54601441b7f61e710de1
nakuwalar <nakuwalar@gmail.com>
2025-06-18 20:57:55 +0700

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [sitemap()]
+});
