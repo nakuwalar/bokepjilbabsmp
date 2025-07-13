b63766f5107edb28b0ce43aa629ecd01b995ecd1
nakuwalar <nakuwalar@gmail.com>
2025-07-13 22:48:54 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Jilbab Smp';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
-export const url = 'https://bokepjilbabsmp.pages.dev';
-export const terbit = '2025-06-23T00:01:03Z';
